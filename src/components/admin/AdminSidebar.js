9b29424c6f24d4868a95d27e1a9934ce38533c6b
sakthiveljayabal0088 <sakthiveljayabal23@gmail.com>
2024-10-17 14:44:09 +0530

    sidebar header created

---
+++ src/components/admin/AdminSidebar.js
@@ -0,0 +1,199 @@
+import React, { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import deals from "../../assets/CRMLogo.png";
+import { BsBarChartFill } from "react-icons/bs";
+import { BiSolidCategory} from "react-icons/bi";
+import { MdCategory } from "react-icons/md";
+import { FaChevronDown, FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { AiOutlineBarChart, AiOutlineShoppingCart } from "react-icons/ai";
+import { FiSettings } from "react-icons/fi";
+import { RiFileList3Line } from "react-icons/ri";
+
+function AdminSidebar({ handleLogout }) {
+  const navigate = useNavigate();
+  const handleLogOutClick = () => {
+    handleLogout();
+    navigate("/");
+  };
+
+  const [activeSubmenu, setActiveSubmenu] = useState(null);
+
+  const toggleSubmenu = (menuName) => {
+    setActiveSubmenu((prev) => (prev === menuName ? null : menuName));
+  };
+
+  return (
+    <nav
+      className="navbar show navbar-vertical h-screen navbar-expand-lg p-0 navbar-light border-bottom border-bottom-lg-0 border-end-lg"
+      id="navbarVertical"
+    >
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler mx-2 p-1"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#sidebarCollapse"
+          aria-controls="sidebarCollapse"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <NavLink
+          className="nav-logo py-lg-2 px-lg-6 m-0 d-flex align-items-center justify-content-start gap-3"
+          to="/"
+        >
+          <img
+            src={deals}
+            alt="deals"
+            className="img-fluid sidebar-logo rounded-circle"
+            style={{
+              background: "#fff",
+              borderRadius: "5px",
+              width: "50px",
+              height: "50px",
+            }}
+          />
+          <p className="text-white">Inventory</p>
+        </NavLink>
+        <div
+          className="collapse navbar-collapse sidebar-bg"
+          id="sidebarCollapse"
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/">
+                <BsBarChartFill className="me-2" />
+                Home
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <div
+                className="nav-link d-flex justify-content-between align-items-center"
+                onClick={() => toggleSubmenu("inventory")}
+              >
+                <span>
+                  <BiSolidCategory className="me-2" />
+                  Inventory
+                </span>
+                {activeSubmenu === "inventory" ? (
+                  <FaChevronDown />
+                ) : (
+                  <FaChevronRight />
+                )}
+              </div>
+              {activeSubmenu === "inventory" && (
+                <ul className="list-unstyled ps-3">
+                  <li>
+                    <NavLink className="nav-link" to="/items">
+                      Items
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink className="nav-link" to="/group-items">
+                      Group Items
+                    </NavLink>
+                  </li>
+                </ul>
+              )}
+            </li>
+
+            <li className="nav-item">
+              <div
+                className="nav-link d-flex justify-content-between align-items-center"
+                onClick={() => toggleSubmenu("sales")}
+              >
+                <span>
+                  <MdCategory className="me-2" />
+                  Sales
+                </span>
+                {activeSubmenu === "sales" ? (
+                  <FaChevronDown />
+                ) : (
+                  <FaChevronRight />
+                )}
+              </div>
+              {activeSubmenu === "sales" && (
+                <ul className="list-unstyled ps-3">
+                  <li>
+                    <NavLink className="nav-link" to="/customers">
+                      Customers
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink className="nav-link" to="/sales-order">
+                      Sales Order
+                    </NavLink>
+                  </li>
+                </ul>
+              )}
+            </li>
+            <li className="nav-item">
+              <div
+                className="nav-link d-flex justify-content-between align-items-center"
+                onClick={() => toggleSubmenu("purchases")}
+              >
+                <span>
+                  <AiOutlineShoppingCart className="me-2" />
+                  Purchases
+                </span>
+                {activeSubmenu === "purchases" ? (
+                  <FaChevronDown />
+                ) : (
+                  <FaChevronRight />
+                )}
+              </div>
+              {activeSubmenu === "purchases" && (
+                <ul className="list-unstyled ps-3">
+                  <li>
+                    <NavLink className="nav-link" to="/vendors">
+                      Vendors
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink className="nav-link" to="/purchase-order">
+                      Purchases Order
+                    </NavLink>
+                  </li>
+                </ul>
+              )}
+            </li>
+
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/integrations">
+                <FiSettings className="me-2" />
+                Integrations
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/reports">
+                <AiOutlineBarChart className="me-2" />
+                Reports
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/documents">
+                <RiFileList3Line className="me-2" />
+                Documents
+              </NavLink>
+            </li>
+          </ul>
+          <div className="mt-auto w-100 mb-4 nav-logo">
+            <button
+              style={{ width: "100%", color: "#fff" }}
+              className="nav-link"
+              onClick={handleLogOutClick}
+            >
+              <FaChevronLeft className="me-2" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default AdminSidebar;
--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -2,14 +2,27 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "../pages/admin/Dashboard";
 import "../styles/admin.css";
+import "../styles/admincdn.css";
+import AdminSidebar from "../components/admin/AdminSidebar";
+import AdminHeader from "../components/admin/AdminHeader";
 function Admin({ handleLogout }) {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Dashboard handleLogout={handleLogout} />} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+          <div className="d-flex flex-column flex-lg-row bg-surface-secondary">
+            <AdminSidebar handleLogout={handleLogout} />
+            <div className="flex-grow-1 h-screen overflow-y-auto">
+              <AdminHeader />
+              <main className="pt-3 bg-surface-secondary">
+                <div style={{ minHeight: "90vh" }}>
+                  <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                  </Routes>
+                </div>
+              </main>
+            </div>
+          </div>
+        </BrowserRouter>
     </div>
   );
 }
